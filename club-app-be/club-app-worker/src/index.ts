1fa5b702ef9295d8c807f21251288120e449efdb
Anthony Oleinik <antholeinik@gmail.com>
2021-07-23 19:42:39 -0500

    chore(worker): moved error_handler into helpers

--- club-app-be/club-app-worker/src/index.ts
+++ club-app-be/club-app-worker/src/index.ts
@@ -1,5 +1,5 @@
 import { router } from './routers'
-import { errorHandler } from './error_handler'
+import { errorHandler } from './helpers/error_handler'
 
 addEventListener('fetch', async (event) => {
   event.respondWith((router.handle(event.request, event) as Promise<Response>).catch(errorHandler))
