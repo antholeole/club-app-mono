248638ddfe41d662a1d2a5a69303d40907652102
Anthony Oleinik <antholeinik@gmail.com>
2021-10-14 11:47:34 -0500

    fix(flutter): fixed bug here reaction menu was not pinned to layer link

--- fe/lib/pages/chat/view/widgets/chats/message/chat_page_message_display.dart
+++ fe/lib/pages/chat/view/widgets/chats/message/chat_page_message_display.dart
@@ -5,16 +5,27 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/src/provider.dart';
 
-class ChatPageMessageDisplay extends StatelessWidget {
-  final LayerLink _link = LayerLink();
-
+class ChatPageMessageDisplay extends StatefulWidget {
   final Message _message;
   final bool? _sentBySelf;
 
-  ChatPageMessageDisplay({required Message message, bool? sentBySelf, Key? key})
+  const ChatPageMessageDisplay(
+      {required Message message, bool? sentBySelf, Key? key})
       : _message = message,
         _sentBySelf = sentBySelf,
         super(key: key);
+
+  @override
+  State<ChatPageMessageDisplay> createState() => _ChatPageMessageDisplayState();
+}
+
+class _ChatPageMessageDisplayState extends State<ChatPageMessageDisplay> {
+  //the layer link MUST be an element of the state;
+  //this is because when this message gets re-rendered, we want to
+  //keep the old layer link as opposed to creating a new one
+  //in order for the overlay to not "lose" this message by
+  //having the old layerLink not have a target.
+  final LayerLink _link = LayerLink();
 
   @override
   Widget build(BuildContext context) {
@@ -23,13 +34,13 @@ class ChatPageMessageDisplay extends StatelessWidget {
       child: GestureDetector(
         onLongPress: () => context
             .read<MessageOverlayCubit>()
-            .addSettingsOverlay(layerLink: _link, message: _message),
+            .addSettingsOverlay(layerLink: _link, message: widget._message),
         onTap: () => context
             .read<MessageOverlayCubit>()
-            .addReactionOverlay(layerLink: _link, message: _message),
+            .addReactionOverlay(layerLink: _link, message: widget._message),
         child: MessageDisplay(
-          message: _message,
-          sentBySelf: _sentBySelf ?? false,
+          message: widget._message,
+          sentBySelf: widget._sentBySelf ?? false,
         ),
       ),
     );
